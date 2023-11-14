783370cc9c2e8f849b7ada7bf606ecb9954ce8b1
Lucas Nasci <lucasnsaraujo0310@gmail.com>
2023-11-14 13:51:00 -0300

    feat: cool animation on star rating

--- src/components/StarRating/index.jsx
+++ src/components/StarRating/index.jsx
@@ -1,7 +1,9 @@
 import { AiFillStar } from "react-icons/ai";
 import { cn } from "../../utils/tailwind";
+import { useState } from "react";
 
 export function StarRating(props) {
+  const [hovering, setHovering] = useState();
   const stars = [
     { value: 1, id: 1 },
     { value: 2, id: 2 },
@@ -17,8 +19,11 @@ export function StarRating(props) {
           <AiFillStar
             key={star.id}
             onClick={() => props?.setExpertise(props?.skillId, star.value)}
+            onMouseOver={() => setHovering(star.value)}
+            onMouseOut={() => setHovering(null)}
             className={cn(
-              "text-xl text-gray-300 hover:text-gray-400 cursor-pointer",
+              "text-xl text-gray-300 hover:text-gray-400 cursor-pointer transition-all",
+              hovering && star.value <= hovering && "text-gray-400",
               props?.expertise >= star.value &&
                 "text-yellow-400 hover:text-yellow-500"
             )}
