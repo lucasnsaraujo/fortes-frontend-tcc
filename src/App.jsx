5e34acd742554579387d0a2fc9b211ae077c587a
Lucas Nasci <lucasnsaraujo0310@gmail.com>
2023-11-14 06:29:19 -0300

    feat: getting user information from localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
-export default function App() {
-  return <></>;
-}
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Login from "./screens/Login";
+import Search from "./screens/Search";
+import Admin from "./screens/Admin";
+import Profile from "./screens/Profile";
+
+export const App = () => {
+  // Check if the user is stored in localStorage
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={user ? <Navigate to="/search" /> : <Login />}
+        />
+        <Route
+          path="/search"
+          element={user ? <Search /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/admin"
+          element={user ? <Admin /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profile"
+          element={user ? <Profile /> : <Navigate to="/login" />}
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./screens/Login";
-import Search from "./screens/Search";
-import Admin from "./screens/Admin";
-import Profile from "./screens/Profile";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/search",
-    element: <Search />,
-  },
-  {
-    path: "/admin",
-    element: <Admin />,
-  },
-  {
-    path: "/profile",
-    element: <Profile />,
-  },
-]);
+import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
