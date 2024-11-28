dcfc1031bfd5c5691164a552319d69763af64f13
Lucas Nasci <lucasnsaraujo0310@gmail.com>
2024-11-27 23:15:02 -0300

    UPDATING TO HTTPS

--- src/services/pocketbase.js
+++ src/services/pocketbase.js
@@ -1,5 +1,5 @@
 import Pocketbase from "pocketbase";
 
-const PROD_URL = "http://pocketbase-production-12bb.up.railway.app";
+const PROD_URL = "https://pocketbase-production-12bb.up.railway.app";
 
 export const pb = new Pocketbase(PROD_URL);
